de545a2cdb7ab71213fafe7a0762b572a8e797f9
Your Name <you@example.com>
2022-02-14 17:07:43 +0100

    vistas

--- src/main/resources/sql/init_db.sql
+++ src/main/resources/sql/init_db.sql
@@ -31,3 +31,9 @@ CREATE TABLE imparten(
     FOREIGN KEY(asignatura) REFERENCES asignaturas(id),
     FOREIGN KEY(alumno) REFERENCES alumnos(num_exp)
 );
+create view profesoresObject as select p.id, dni, p.nombre 'nombre', apellidos, d.nombre 'departamento', p.departamento 'id_departamento'
+from profesores p inner join departamentos d on p.departamento = d.id;
+
+create view departamentosObject as select d.id, d.nombre, count(p.id) 'num_prof'
+from departamentos d left join profesores p on d.id = p.departamento
+group by d.id;
